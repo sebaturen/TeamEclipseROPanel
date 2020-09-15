d1aee27a4f66b26302cd11e278a3076ada914c1f
Sebastián Turén Croquevielle <seba@turensoft.com>
2020-09-15 18:07:11 -0300

    render body and headr beta1

--- src/main/java/com/eclipse/panel/viewController/CharacterController.java
+++ src/main/java/com/eclipse/panel/viewController/CharacterController.java
@@ -1,13 +1,12 @@
 package com.eclipse.panel.viewController;
 
-import com.eclipse.panel.gameObject.character.Character;
-import com.eclipse.panel.viewController.rest.Guilds;
 import com.eclipse.panel.viewController.roRender.ROFrame;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URL;
 import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
@@ -25,10 +24,6 @@ public class CharacterController {
     private static final Properties jobsNameProp = new Properties();
     private static final Properties sexProp = new Properties();
     private static ClassLoader classLoader;
-
-    // JOB SPRITE INFO
-    private static final int JOB_HEADER_START =  0;
-    private static final int JOB_TOTAL_FRAME_START = 4;
 
     static {
         try {
@@ -50,53 +45,53 @@ public class CharacterController {
         int job = 0;
         int head = 0;
         int sex = 0;
-        int palette = 0;
-        String jobSpriteAct = RO_SPRITES_LOC + JOB_LOC + jobsNameProp.getProperty(job+"") +"_"+ sexProp.getProperty(sex+"");
+        int bodyPalette = 1;
+        int headPalette = 2;
+        int frame = 0;
+
+        BufferedImage bodyImg = getBodySprite(
+                job+"",
+                sex+"",
+                bodyPalette,
+                frame
+        );
+        ImageIO.write(bodyImg, "png", new File("body.png"));
+
+        BufferedImage headImg = getHeadSprite(
+                head+"",
+                sex+"",
+                headPalette,
+                frame
+        );
+        ImageIO.write(headImg, "png", new File("head.png"));
+
+    }
+
+    private static BufferedImage getBodySprite(String jobId, String sexId, int bodyPalette, int postActor) throws IOException {
+
+        String jobSpriteAct = RO_SPRITES_LOC + JOB_LOC + jobsNameProp.getProperty(jobId) +"_"+ sexProp.getProperty(sexId);
+        String palettePal = RO_SPRITES_LOC + JOB_PALETTE + jobsNameProp.getProperty(jobId) +"_"+ sexProp.getProperty(sexId) +"_"+ bodyPalette;
 
         URL jobSprite = classLoader.getResource(jobSpriteAct+".spr");
-        if (jobSprite != null) {
-
-            byte[] jobSpriteSPR;
-            try (InputStream fis = jobSprite.openStream();
-                 ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
-
-                byte[] buff = new byte[8000];
-                int bytesRead = 0;
-
-                while((bytesRead = fis.read(buff)) != -1) {
-                    buffer.write(buff, 0, bytesRead);
-                }
-
-                jobSpriteSPR = buffer.toByteArray();
-            }
-
-            if (jobSpriteSPR != null) {
-                byte[] bTotalFrames = reverseContent(Arrays.copyOfRange(jobSpriteSPR, JOB_TOTAL_FRAME_START, JOB_TOTAL_FRAME_START+4));
-                int totalFrames = (ByteBuffer.wrap(bTotalFrames)).getInt();
-                int bytePosition = JOB_TOTAL_FRAME_START+4;
-                List<ROFrame> framesList = new ArrayList<>();
-                while (totalFrames != 0) {
-                    byte[] bSizeX = reverseContent(Arrays.copyOfRange(jobSpriteSPR, bytePosition, bytePosition+2));
-                    byte[] bSizeY = reverseContent(Arrays.copyOfRange(jobSpriteSPR, bytePosition+2, bytePosition+4));
-                    byte[] bByteSize = reverseContent(Arrays.copyOfRange(jobSpriteSPR, bytePosition+4, bytePosition+6));
-
-                    short sizeX = (ByteBuffer.wrap(bSizeX)).getShort();
-                    short sizeY = (ByteBuffer.wrap(bSizeY)).getShort();
-                    short byteSize = (ByteBuffer.wrap(bByteSize)).getShort();
-                    byte[] bSprite = Arrays.copyOfRange(jobSpriteSPR, bytePosition+6, bytePosition+6+byteSize);
-                    framesList.add(new ROFrame(sizeX, sizeY, bSprite));
-
-                    bytePosition += 6 + byteSize;
-                    totalFrames--;
-                }
-                byte[] bPalette = Arrays.copyOfRange(jobSpriteSPR, bytePosition, jobSpriteSPR.length);
-                framesList.get(0).getPng(bPalette);
-                //System.out.println(convertBytesToHex(jobSpriteSPR));
-            }
+        URL palette = null;
+        if (bodyPalette != 0) {
+            palette = classLoader.getResource(palettePal+".pal");
         }
+        return ROFrame.processSPR(jobSprite, palette, postActor);
+    }
 
-        //FileInputStream bodySpr = new FileInputStream(Objects.requireNonNull(classLoader.getResource(jobSprite)).getFile());
-        //System.out.println(bodySpr);
+    private static BufferedImage getHeadSprite(String headId, String sexId, int headPalette, int postHead) throws IOException {
+
+        String headSpriteAct = RO_SPRITES_LOC + HEADS_LOC + headsProp.getProperty(headId) +"_"+ sexProp.getProperty(sexId);
+        String palettePal = RO_SPRITES_LOC + HEADS_PALETTE +"머리"+ headsProp.getProperty(headId) +"_"+ sexProp.getProperty(sexId) +"_"+ headPalette;
+
+        URL headSprite = classLoader.getResource(headSpriteAct+".spr");
+        URL palette = null;
+        if (headPalette != 0) {
+            palette = classLoader.getResource(palettePal+".pal");
+            System.out.println(palettePal);
+        }
+        return ROFrame.processSPR(headSprite, palette, postHead);
 
     }
 
@@ -104,33 +99,4 @@ public class CharacterController {
         renderCharacter(1);
     }
 
-    /**
-     * Convert byte[] -> String [AA BB CC ...]
-     * @param bytes
-     * @return
-     */
-    public static String convertBytesToHex(byte[] bytes) {
-
-        StringBuilder result = new StringBuilder();
-
-        for (byte temp : bytes) {
-            int decimal = (int) temp & 0xff;  // bytes widen to int, need mask, prevent sign extension
-            String hex = Integer.toHexString(decimal).toUpperCase();
-            if (hex.length() == 1) {
-                hex = "0"+ hex;
-            }
-            result.append(hex);
-        }
-        return result.toString();
-
-    }
-
-    public static byte[] reverseContent(byte[] content) {
-        for(int i = 0; i < content.length / 2; i++) {
-            byte t = content[i];
-            content[i] = content[content.length -1 -i];
-            content[content.length -1 -i] = t;
-        }
-        return content;
-    }
 }
--- src/main/java/com/eclipse/panel/viewController/roRender/ROFrame.java
+++ src/main/java/com/eclipse/panel/viewController/roRender/ROFrame.java
@@ -1,5 +1,13 @@
 package com.eclipse.panel.viewController.roRender;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -47,10 +55,110 @@ public class ROFrame {
         return bMapName;
     }
 
-    public void getPng(byte[] palette) {
+    public BufferedImage getPng(byte[] palette) {
         uncompress();
-        // apply palette
+        // Prepare palette
+        List<byte[]> rgbPalette = new ArrayList<>();
+        for(int i = 0; i < palette.length; i+=4) {
+            byte[] rgbIndex = Arrays.copyOfRange(palette, i, i+4);
+            rgbPalette.add(rgbIndex);
+        }
+        // Prepare image
+        BufferedImage dest = new BufferedImage(sizeX, sizeY, BufferedImage.TYPE_INT_ARGB);
+        int x,y = x = 0;
+        for(int i = 0; i < frame.length; i++) {
+            if (i > 0 && i%sizeX == 0) {
+                x = 0;
+                y++;
+            }
+            int paletteColorPos = (int) frame[i] & 0xff;
+            byte[] rgb = rgbPalette.get(paletteColorPos);
+            Color c = new Color((int) rgb[0] & 0xff, (int) rgb[1] & 0xff, (int) rgb[2] & 0xff, (paletteColorPos == 0)? 0:255);
+            dest.setRGB(x, y, c.getRGB());
+            x++;
+        }
+
+        return dest;
         
     }
+
+    // JOB SPRITE INFO
+    private static final int JOB_HEADER_START =  0;
+    private static final int JOB_TOTAL_FRAME_START = 4;
+
+    public static BufferedImage processSPR(URL sprite, URL palette, int spritePosition) throws IOException {
+
+        byte[] spriteSPR;
+        byte[] orgPalette = null;
+        byte[] altPalette = null;
+        List<ROFrame> framesList = new ArrayList<>();
+        spriteSPR = urlToByte(sprite);
+        if (palette != null) {
+            altPalette = urlToByte(palette);
+        }
+
+        if (spriteSPR != null) {
+            byte[] bTotalFrames = reverseContent(Arrays.copyOfRange(spriteSPR, JOB_TOTAL_FRAME_START, JOB_TOTAL_FRAME_START+4));
+            int totalFrames = (ByteBuffer.wrap(bTotalFrames)).getInt();
+            int bytePosition = JOB_TOTAL_FRAME_START+4;
+            while (totalFrames != 0) {
+                byte[] bSizeX = reverseContent(Arrays.copyOfRange(spriteSPR, bytePosition, bytePosition+2));
+                byte[] bSizeY = reverseContent(Arrays.copyOfRange(spriteSPR, bytePosition+2, bytePosition+4));
+                byte[] bByteSize = reverseContent(Arrays.copyOfRange(spriteSPR, bytePosition+4, bytePosition+6));
+
+                short sizeX = (ByteBuffer.wrap(bSizeX)).getShort();
+                short sizeY = (ByteBuffer.wrap(bSizeY)).getShort();
+                short byteSize = (ByteBuffer.wrap(bByteSize)).getShort();
+                framesList.add(new ROFrame(sizeX, sizeY, Arrays.copyOfRange(spriteSPR, bytePosition+6, bytePosition+6+byteSize)));
+
+                bytePosition += 6 + byteSize;
+                totalFrames--;
+            }
+            orgPalette = Arrays.copyOfRange(spriteSPR, bytePosition, spriteSPR.length);
+        }
+
+        if (framesList.size() >= spritePosition) {
+            byte[] sendPalette;
+            if (palette != null) {
+                sendPalette = altPalette;
+            } else {
+                sendPalette = orgPalette;
+            }
+            return framesList.get(spritePosition).getPng(sendPalette);
+        }
+
+        return null;
+
+    }
+
+    private static byte[] reverseContent(byte[] content) {
+        for(int i = 0; i < content.length / 2; i++) {
+            byte t = content[i];
+            content[i] = content[content.length -1 -i];
+            content[content.length -1 -i] = t;
+        }
+        return content;
+    }
+
+    private static byte[] urlToByte(URL elem) throws IOException {
+        byte[] outByte;
+        try (InputStream fis = elem.openStream();
+             ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
+
+            byte[] buff = new byte[8000];
+            int bytesRead = 0;
+
+            while((bytesRead = fis.read(buff)) != -1) {
+                buffer.write(buff, 0, bytesRead);
+            }
+
+            outByte = buffer.toByteArray();
+        }
+
+        return outByte;
+    }
+
+
+
 
 }
