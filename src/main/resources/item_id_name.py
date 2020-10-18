fa9b4c437e58fa24c2bb99dacd836283f81bb005
Sebastián Turén Croquevielle <sebaturen@gmail.com>
2020-10-18 15:19:45 -0300

    fix item info

--- src/main/resources/item_id_name.py
+++ src/main/resources/item_id_name.py
@@ -1,9 +1,11 @@
 # Extract skill description from data.grf
 
 FILE = 'items'
+listItemsSee = []
 
 with open(FILE) as fp:
     for line in fp:
         info = line.split("#")
-        if (len(info) > 2):
+        if (len(info) > 2 and info[0] not in listItemsSee):
             print("{}={}".format(info[0], info[1]))
+            listItemsSee.append(info[0])
